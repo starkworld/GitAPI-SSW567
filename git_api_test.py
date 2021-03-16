c53194ffbf83da10fa2e1f3fdc9de9eb288f0020
stark world <nicky.kalyan438@gmail.com>
2021-03-16 17:43:05 -0400

    HW05_Mocking

--- git_api_test.py
+++ git_api_test.py
@@ -1,29 +1,29 @@
+"""
+Unittest cases for testing github API, and isolating external dependencies.
+Author: NK
+Date: 03/15/2021
+"""
+
+
 import unittest
 from git_api import get_user_repos, get_commit_counts
+from unittest.mock import MagicMock
 
 
 class TestGitHubRepo(unittest.TestCase):
     """Test cases for github repo program"""
-    def testGitHub01(self):
+
+    def testUserRepo(self):
+        mock = MagicMock()
+        mock.__str__.return_value = '[hellogitworld, helloworld, Mocks, Project1, threads-of-life]'
         self.assertIn("SSW-567", get_user_repos("starkworld"))
+        self.assertEqual(str(mock), '[hellogitworld, helloworld, Mocks, Project1, threads-of-life]')
 
-    def testGitHub02(self):
-        self.assertNotIn("HelloWorld", get_user_repos("starkworld"))
-
-    def testGitHub03(self):
+    def testGetCommitCounts(self):
+        mock = MagicMock()
+        mock.__str__.return_value = '30'
+        self.assertEqual(str(mock), '30')
         self.assertEqual(20, get_commit_counts("starkworld", "Student_DB_Repository"))
-
-    def testGitHub04(self):
-        self.assertIn("Future-Sales-Prediction", get_user_repos("starkworld"))
-
-    def testGitHub05(self):
-        self.assertNotIn("Empire", get_user_repos("starkworld"))
-
-    def testGitHub06(self):
-        self.assertNotEqual(42, get_commit_counts("starkworld", "SSW-567"))
-
-    def testGitHub07(self):
-        self.assertEqual([], get_user_repos("FakeUser099"))
 
 
 if __name__ == '__main__':
